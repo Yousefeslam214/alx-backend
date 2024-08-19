b65b17b529197fa753accadb454e32eb8f29b477
Yousef eslam <yousefeslam214@gmail.com>
2024-08-19 09:57:22 +0300

    update

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,4 +5,4 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     
     size = (page-1)* page_size
-    return[size, size + page_size]
+    return(size, size + page_size)
