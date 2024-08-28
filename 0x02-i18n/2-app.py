b296aa25321aff12211c0be3a3dbae147ce28fc6
Yousef eslam <yousefeslam214@gmail.com>
2024-08-28 06:13:29 +0300

    update

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Task 0: Basic Flask app
+'''Task 2: Basic Flask app
 '''
 
 from flask import Flask, render_template
@@ -8,6 +8,7 @@ from flask_babel import Babel
 
 class Config:
     '''Config class'''
+    DEBUG = True
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -17,15 +18,24 @@ class Config:
 app = Flask(__name__)
 app.config.from_object(Config)
 app.url_map.strict_slashes = False
-
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+
+    Returns:
+        str: best match
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index():
     '''default route'''
-    return render_template("1-index.html")
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
